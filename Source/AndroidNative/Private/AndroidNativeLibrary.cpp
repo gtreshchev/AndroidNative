6226c938c35e62cb3c622e4e40d73d0fe21afa38
Georgy Treshchev <gtreshchev@gmail.com>
2023-01-01 15:29:40 +0400

    Year update from 2022 to 2023

--- Source/AndroidNative/Private/AndroidNativeLibrary.cpp
+++ Source/AndroidNative/Private/AndroidNativeLibrary.cpp
@@ -1,4 +1,4 @@
-// Georgy Treshchev 2022.
+// Georgy Treshchev 2023.
 
 #include "AndroidNativeLibrary.h"
 #include "AndroidNative.h"
--- Source/AndroidNative/Private/Java/DeviceInfo.java
+++ Source/AndroidNative/Private/Java/DeviceInfo.java
@@ -1,4 +1,4 @@
-// Georgy Treshchev 2022.
+// Georgy Treshchev 2023.
 
 package com.Plugins.AndroidNative;
 
