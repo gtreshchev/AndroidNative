55339db23a517480c3e43b875d351612a1412dbf
Georgy Treshchev <gtreshchev@gmail.com>
2023-10-14 09:35:38 +0100

    Expose LogAndroidNative externally to make it possible for the AndroidNativeUtils functionality to be called outside of the AndroidNative module

--- Source/AndroidNative/Public/AndroidNativeDefines.h
+++ Source/AndroidNative/Public/AndroidNativeDefines.h
@@ -9,7 +9,7 @@
 #include "Logging/LogMacros.h"
 #include "Logging/LogVerbosity.h"
 
-DECLARE_LOG_CATEGORY_EXTERN(LogAndroidNative, Log, All);
+ANDROIDNATIVE_API DECLARE_LOG_CATEGORY_EXTERN(LogAndroidNative, Log, All);
 
 /**
  * Convenient approach for "overloading" functions by return type with the same arguments. Achieved thanks to SFINAE.
